a8e0d7bc113853d0ac405373fbc713b814faf262
VanderSpeare <125333024+VanderSpeare@users.noreply.github.com>
2023-03-08 00:30:53 +0900

    Add files via upload

---
+++ HopSua.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    struct HopSua
+    {
+        char NhanHieu[20];
+        int TrongLuong;
+        time_t time(time_t *time);
+    };
+    typedef struct Nhan;
+    void NhapNhan(Nhan &t);
+    {
+        cout <<"nhap nhan hieu:";
+        cin <<Nhan;
+        cout << "nhap trong luong";
+        cin << TrongLuong;
+        cout <<"350g"
+    };
+    
+    
+    system("pause");
+    return 0;
+}
